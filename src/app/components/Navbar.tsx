b564e368b736a777383e14c4ba50993b4b08e148
220064@YSE <220064md@yse-c.net>
2024-01-17 10:20:37 +0900

    Update package versions and add React Icons. (generated by blackbox.ai )

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,17 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { SiLoopback } from 'react-icons/si';
 
 const Navbar = () => {
+    console.log('Navbar!!!')
     return (
         <nav>
-            <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                <Link href="/" className="ms-3 me-3">
-                    <span className='self-center text-2xl font-semibold'>Next Tweet</span>
-                </Link>
+            <div className='flex px-5 py-3 border-b'>
+                <div className="flex items-center mr-6">
+                    <SiLoopback className="me-3" size="2em" />
+                    <Link href="/" className='ms-3 me-3'>
+                        <span className='self-center text-2xl font-semibold'>Next Tweet</span>
+                    </Link>
+                </div>
 
                 <div>
                     <ul className='flex flex-wrap'>
@@ -23,7 +28,8 @@ const Navbar = () => {
                             <Link href="/user/logout" className='py-2 px-3'>Sign out</Link>
                         </li>
                     </ul>
-                </div>            </div>
+                </div>
+            </div>
         </nav>
     );
 }
