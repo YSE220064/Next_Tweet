c7717de2a704a38bfd75293d71af03c8eb7cdd07
220064@YSE <220064md@yse-c.net>
2024-01-24 10:37:04 +0900

    src app page import etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+"use client"
+
+import { useState } from "react";
+import { User, testUser } from "./models/User";
+import { Tweet } from "./models/Tweet";
 import Image from 'next/image'
 
 export default function Home() {
+  //テストユーザの取得
+  const [user] = useState<User>(testUser);
+  const [tweets, setTweets] = useState<Tweet[]>([]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
